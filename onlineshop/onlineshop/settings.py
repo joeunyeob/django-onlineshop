196c1e964172b7eedeb854ca8b2de6bc8fbc64ac
조은엽 <jo-eun-yeob@jo-eun-yeob-ui-MacBookPro-3.local>
2019-08-12 21:08:22 +0900

    cart app add

--- onlineshop/onlineshop/settings.py
+++ onlineshop/onlineshop/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.naver',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +126,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+
+
+CART_ID = 'cart_in_session'
--- onlineshop/shop/models.py
+++ onlineshop/shop/models.py
@@ -22,23 +22,26 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='product')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='products')
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True, unique=True, allow_unicode=True)
-    image = models.ImageField(upload_to='product/%Y/$m/$d', blank=True)
 
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/%Y/%m/%d',blank=True)
+    description = models.TextField(blank=True)
+    meta_description = models.TextField(blank=True)
+
+    price = models.DecimalField(max_digits=10,decimal_places=2)
     stock = models.PositiveIntegerField()
 
     available_display = models.BooleanField('Display', default=True)
-    available_order = models.DateTimeField(auto_now=True)
+    available_order = models.BooleanField('Order', default=True)
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['-created']
-        index_together = [['id', 'slug']]
+        index_together = [['id','slug']]
 
     def __str__(self):
         return self.name
